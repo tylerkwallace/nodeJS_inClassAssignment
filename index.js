641110bfaf1524c5d0543538222c2e9ace49854f
tylerkwallace <wallace_tyler1@columbusstate.edu>
2021-11-09 01:25:59 +0000

    debug

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended : true}));
 
 app.get('/', function(request, response){
-    response.send('Hello World!')
+    response.render('index');
+});
+
+app.post('/addToDo', function(req, res){
+    res.send('hello');
+});
+
+app.post('/removeToDo', function(req, res){
+    res.send('world');
 });
 
 app.listen(3000, function(){
