884ae43bbc5573076f571666b754513b9adad3e3
tylerkwallace <wallace_tyler1@columbusstate.edu>
2021-11-10 00:27:49 +0000

    implement button/form functionality

--- index.js
+++ index.js
@@ -5,15 +5,32 @@ var app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended : true}));
 
+var tasks = ['wake up', 'eat breakfast'];
+var completed = [];
+
 app.get('/', function(request, response){
-    response.render('index');
+    response.render('index', {tasks: tasks, completed: completed});
 });
 
 app.post('/addToDo', function(req, res){
-    res.send('hello');
+    console.log(req)
+    tasks.push(req.body.newtodo)
+    res.redirect('/');
 });
 
 app.post('/removeToDo', function(req, res){
+    const remove = req.body.check;
+    if(typeof remove === 'string'){
+        tasks.splice(tasks.indexOf(remove),1)
+    } else if(typeof remove === "object"){
+        for( var i=0; i< remove.length; i++){
+            tasks.splice(tasks.indexOf(remove[i]),1)
+        }
+    }
+    res.redirect('/')
+});
+
+app.post('/deleteToDo', function(req, res){
     res.send('world');
 });
 
