2da6a96987ab9c323e0fe3d5791af5a5d57015af
tylerkwallace <wallace_tyler1@columbusstate.edu>
2021-11-14 19:04:17 +0000

    implement delete

--- index.js
+++ index.js
@@ -14,27 +14,26 @@ mongoose.Promise = global.Promise;
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:   '));
 
-let tasks = ['wake up', 'eat breakfast', 'brs'];
+let tasks = [];
 let completed = []; 
 
 app.get('/', function(request, response){
-    // ToDo.find(function(err, todo){
-    //     if(err){
-    //         console.log(err);
-    //     } else{
-    //         tasks = [];
-    //         completed = [];
-    //         for( let i=0; i<todo.length; i++){
-    //             if(todo[i].done){
-    //                 completed.push(todo[i]);
-    //             } else {
-    //                 tasks.push(todo[i]);
-    //             }
-    //         }
-    //         response.render('index', {tasks: tasks, completed: completed});
-    //     }
-    // })
-    response.render('index', {tasks: tasks, completed: completed});
+    ToDo.find(function(err, todo){
+        if(err){
+            console.log(err);
+        } else{
+            tasks = [];
+            completed = [];
+            for( let i=0; i<todo.length; i++){
+                if(todo[i].done){
+                    completed.push(todo[i]);
+                } else {
+                    tasks.push(todo[i]);
+                }
+            }
+            response.render('index', {tasks: tasks, completed: completed});
+        }
+    })
 });
 
 app.post('/addToDo', function(req, res){
@@ -54,7 +53,7 @@ app.post('/addToDo', function(req, res){
 app.post('/removeToDo', function(req, res){
     const remove = req.body.check;
     if(typeof remove === 'string'){
-        ToDo.updateOne({item:remove}, {done:true}, function(err){
+        ToDo.updateOne({_id:remove}, {done:true}, function(err){
             if(err){
                 console.log(err);
             } else {
@@ -62,29 +61,36 @@ app.post('/removeToDo', function(req, res){
             }
         })
     } else if(typeof remove === "object"){
-        for( let i=0; i< remove.length; i++){
-            ToDo.updateOne({item:remove[i]}, {done:true}, function(err){
+        for( var i=0; i< remove.length; i++){
+            ToDo.updateOne({_id:remove[i]}, {done:true}, function(err){
                 if(err){
                     console.log(err);
-                } else {
-                    res.redirect('/');
                 }
             })
         }
-        res.redirect('/')
+        res.redirect('/');
     }
 });
 
 app.post('/deleteToDo', function(req, res){
     const deleteTask = req.body.delete;
     if(typeof deleteTask === 'string'){
-        completed.splice(completed.indexOf(deleteTask),1)
+        ToDo.deleteOne({_id: deleteTask}, function(err){
+            if(err){
+                console.log(err);
+            }
+            res.redirect('/')
+        })
     } else if(typeof deleteTask === "object"){
         for( let i=0; i< deleteTask.length; i++){
-            completed.splice(completed.indexOf(deleteTask[i]),1);
+            ToDo.deleteOne({_id: deleteTask[i]}, function(err){
+                if(err){
+                    console.log(err);
+                }
+            })
         }
+        res.redirect('/');
     }
-    res.redirect('/')
 });
 
 app.listen(3000, function(){
