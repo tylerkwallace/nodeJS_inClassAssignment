20817d7f84299c98fa2981e10625dd3879bb0abb
tylerkwallace <wallace_tyler1@columbusstate.edu>
2021-11-14 21:47:55 +0000

    zip

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ let express = require('express');
 let bodyParser = require('body-parser');
 let mongoose = require('mongoose');
 const ToDo = require('./models/todo.model');
+//let fetch = require('node-fetch');
 
 let app = express();
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //connection to mongo
 const mongoDB = 'mongodb+srv://admin:admin@cluster0.1artz.mongodb.net/todo?retryWrites=true&w=majority'
@@ -15,55 +16,55 @@ let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:   '));
 
 let tasks = [];
-let completed = []; 
+let completed = [];
 
-app.get('/', function(request, response){
-    ToDo.find(function(err, todo){
-        if(err){
+app.get('/', function (request, response) {
+    ToDo.find(function (err, todo) {
+        if (err) {
             console.log(err);
-        } else{
+        } else {
             tasks = [];
             completed = [];
-            for( let i=0; i<todo.length; i++){
-                if(todo[i].done){
+            for (let i = 0; i < todo.length; i++) {
+                if (todo[i].done) {
                     completed.push(todo[i]);
                 } else {
                     tasks.push(todo[i]);
                 }
             }
-            response.render('index', {tasks: tasks, completed: completed});
+            response.render('index', { tasks: tasks, completed: completed });
         }
     })
 });
 
-app.post('/addToDo', function(req, res){
+app.post('/addToDo', function (req, res) {
     let newTodo = new ToDo({
         item: (req.body.newtodo),
         done: false
     })
-    newTodo.save(function(err, todo){
-        if(err){
+    newTodo.save(function (err, todo) {
+        if (err) {
             console.log(err);
         } else {
             res.redirect('/');
         }
-    });    
+    });
 });
 
-app.post('/removeToDo', function(req, res){
+app.post('/removeToDo', function (req, res) {
     const remove = req.body.check;
-    if(typeof remove === 'string'){
-        ToDo.updateOne({_id:remove}, {done:true}, function(err){
-            if(err){
+    if (typeof remove === 'string') {
+        ToDo.updateOne({ _id: remove }, { done: true }, function (err) {
+            if (err) {
                 console.log(err);
             } else {
                 res.redirect('/');
             }
         })
-    } else if(typeof remove === "object"){
-        for( var i=0; i< remove.length; i++){
-            ToDo.updateOne({_id:remove[i]}, {done:true}, function(err){
-                if(err){
+    } else if (typeof remove === "object") {
+        for (var i = 0; i < remove.length; i++) {
+            ToDo.updateOne({ _id: remove[i] }, { done: true }, function (err) {
+                if (err) {
                     console.log(err);
                 }
             })
@@ -82,7 +83,7 @@ app.post('/deleteToDo', function(req, res){
             res.redirect('/')
         })
     } else if(typeof deleteTask === "object"){
-        for( let i=0; i< deleteTask.length; i++){
+        for(var i=0; i<deleteTask.length; i++){
             ToDo.deleteOne({_id: deleteTask[i]}, function(err){
                 if(err){
                     console.log(err);
@@ -91,6 +92,16 @@ app.post('/deleteToDo', function(req, res){
         }
         res.redirect('/')
     }
+})
+
+app.get('/zip', function (req, res) {
+    res.render('zip');
+    // res.render('zip', { tasks: tasks, completed: completed });
+});
+
+app.post('/zip', function (req, res) {
+    res.render('zip');
+    // res.render('zip', { tasks: tasks, completed: completed });
 });
 
 app.listen(3000, function(){
