17f66fa1e6cc555383916ebd2bfb69b76692a851
tylerkwallace <wallace_tyler1@columbusstate.edu>
2021-11-14 18:21:52 +0000

    connect mongoDB

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+let express = require('express');
+let bodyParser = require('body-parser');
+let mongoose = require('mongoose');
+const ToDo = require('./models/todo.model');
 
-var app = express();
+let app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended : true}));
 
-let tasks = [];
-let completed = [];
+//connection to mongo
+const mongoDB = 'mongodb+srv://admin:admin@cluster0.1artz.mongodb.net/todo?retryWrites=true&w=majority'
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:   '));
+
+let tasks = ['wake up', 'eat breakfast', 'brs'];
+let completed = []; 
 
 app.get('/', function(request, response){
     // ToDo.find(function(err, todo){
@@ -29,23 +38,41 @@ app.get('/', function(request, response){
 });
 
 app.post('/addToDo', function(req, res){
-    console.log(req)
-    tasks.push(req.body.newtodo)
-    res.redirect('/');
+    let newTodo = new ToDo({
+        item: (req.body.newtodo),
+        done: false
+    })
+    newTodo.save(function(err, todo){
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect('/');
+        }
+    });    
 });
 
 app.post('/removeToDo', function(req, res){
     const remove = req.body.check;
     if(typeof remove === 'string'){
-        tasks.splice(tasks.indexOf(remove),1)
-        completed.push(remove);
+        ToDo.updateOne({item:remove}, {done:true}, function(err){
+            if(err){
+                console.log(err);
+            } else {
+                res.redirect('/');
+            }
+        })
     } else if(typeof remove === "object"){
-        for( var i=0; i< remove.length; i++){
-            tasks.splice(tasks.indexOf(remove[i]),1);
-            completed.push(remove[i])
+        for( let i=0; i< remove.length; i++){
+            ToDo.updateOne({item:remove[i]}, {done:true}, function(err){
+                if(err){
+                    console.log(err);
+                } else {
+                    res.redirect('/');
+                }
+            })
         }
+        res.redirect('/')
     }
-    res.redirect('/')
 });
 
 app.post('/deleteToDo', function(req, res){
@@ -53,7 +80,7 @@ app.post('/deleteToDo', function(req, res){
     if(typeof deleteTask === 'string'){
         completed.splice(completed.indexOf(deleteTask),1)
     } else if(typeof deleteTask === "object"){
-        for( var i=0; i< deleteTask.length; i++){
+        for( let i=0; i< deleteTask.length; i++){
             completed.splice(completed.indexOf(deleteTask[i]),1);
         }
     }
